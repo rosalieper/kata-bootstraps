2246ca3190df5236164c6df84b3ea6843fe82712
rosalie <perside.rosalie@wikimedia.de>
2019-11-26 00:54:35 +0100

    Added passing test and a failing test

--- php/src/Kata.php
+++ php/src/Kata.php
@@ -2,7 +2,39 @@
 
 namespace swkberlin;
 
-class Kata
-{
+class Kata {
 
+    private $specialNumbers = [
+      "I" => 1,
+      "V" => 5,
+      "X" => 10,
+      "L" => 50,
+      "C" => 100,
+      "D" => 500,
+      "M" => 1000
+    ];
+
+    public function charRepeat( $char, $times ) {
+       $rom ="";
+        for( $i = 1; $i <= $times; $i++ ){
+            $rom = $char . $rom;
+        }
+        return $rom;
+    }
+    public function getSpecialRoman( $num ) {
+        if ( in_array( $num, $this->specialNumbers ) ) {
+            return array_search( $num, $this->specialNumbers );
+        }
+    }
+
+    public function toRoman ( $num ){
+        $splitNum = [];
+        $i = 0;
+        while( $num != 0 ) {
+            $splitNum = $num%10;
+            $num = $num/10;
+        }
+
+
+    }
 }
